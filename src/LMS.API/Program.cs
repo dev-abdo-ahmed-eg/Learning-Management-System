8c8d1be01c8c1efa61e473033b55d9aee3f48997
abdulrahman.mahmoud <abdulrahman.mahmoud@ABDULRAHMAN-MAH>
2026-06-29 22:25:26 +0300

    Add event bus and DI services for LMS application
    Enhanced the LMS application by introducing an event bus and
    registering related services in the dependency injection container.
    Added `NotificationSubscriber`, `ProgressSubscriber`, and
    `AuditSubscriber` with event bus subscriptions. Updated `Program.cs`
    to enable Swagger in development. Included a new NuGet package
    reference for `Microsoft.Extensions.Logging.Abstractions` in
    `LMS.Application.csproj`.

--- src/LMS.API/Program.cs
+++ src/LMS.API/Program.cs
@@ -1,5 +1,7 @@
+using LMS.Application.Events;
 using LMS.Application.Factory;
 using LMS.Application.Services;
+using LMS.Application.Subscribers;
 using LMS.Domain.Interfaces;
 using LMS.Infrastructure.Repositories;
 
@@ -8,12 +10,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IAssignmentRepository, InMemoryAssignmentRepository>();
 builder.Services.AddSingleton<ISubmissionRepository, InMemorySubmissionRepository>();
 builder.Services.AddSingleton<IGradingStrategyFactory, GradingStrategyFactory>();
+builder.Services.AddSingleton<IEventBus, InProcessEventBus>();
 builder.Services.AddSingleton<GradingService>();
+builder.Services.AddSingleton<NotificationSubscriber>();
+builder.Services.AddSingleton<ProgressSubscriber>();
+builder.Services.AddSingleton<AuditSubscriber>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+var bus = app.Services.GetRequiredService<IEventBus>();
+bus.Subscribe(app.Services.GetRequiredService<NotificationSubscriber>());
+bus.Subscribe(app.Services.GetRequiredService<ProgressSubscriber>());
+bus.Subscribe(app.Services.GetRequiredService<AuditSubscriber>());
 
 if (app.Environment.IsDevelopment())
 {
